7546986dae3140bcac0bdcd845d695c5538ae56c
August Sturm <auguststurm@users.noreply.github.com>
2020-08-13 12:31:22 -0700

    Updates

---
+++ precache-manifest.6f4a5646ed770a35000ce73054f9e4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286f52ed72234b295de5485e358f21ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92db8155aae86167304a",
+    "url": "./static/css/main.e38afaa8.chunk.css"
+  },
+  {
+    "revision": "9bdcb08d29e9aaf788b4",
+    "url": "./static/js/2.08033326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.08033326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92db8155aae86167304a",
+    "url": "./static/js/main.8b45177d.chunk.js"
+  },
+  {
+    "revision": "c46c974de80fc38d0246",
+    "url": "./static/js/runtime-main.33bef97d.js"
+  }
+]);
