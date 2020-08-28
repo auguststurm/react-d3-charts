61d53cd50fabaeb1d1a5dcb8b49e61f61756e7e1
August Sturm <auguststurm@users.noreply.github.com>
2020-08-27 23:17:32 -0700

    decoupled naming

--- src/BarChart/bar-chart.text.jsx
+++ src/BarChart/bar-chart.text.jsx
@@ -4,16 +4,16 @@ import { dataExampleSpecs } from '../../example/src/data';
 import cheerio from 'cheerio';
 import BarChart from '.';
 
-const barChartProps = dataExampleSpecs.appComponentStates.barChart;
 
 describe('BarChart component', () => {
 
   let component, $;
+  const componentProps = dataExampleSpecs.appComponentStates.barChart;
   const element = document.createElement('div');
 
   beforeAll((done) => {
     document.body.appendChild(element);
-    component = ReactDOM.render(<BarChart {...barChartProps} />, element);
+    component = ReactDOM.render(<BarChart {...componentProps} />, element);
     setTimeout(function () {
       done();
       $ = cheerio.load(element.innerHTML);
@@ -24,20 +24,20 @@ describe('BarChart component', () => {
     document.body.removeChild(element);
   });
 
-  it(`svg element is ${barChartProps.width} wide`, () => {
+  it(`svg element is ${componentProps.width} wide`, () => {
     const width = $('svg').attr('width');
-    expect(width).toEqual(barChartProps.width.toString());
+    expect(width).toEqual(componentProps.width.toString());
   });
 
-  it(`svg element is ${barChartProps.height} high`, () => {
+  it(`svg element is ${componentProps.height} high`, () => {
     const height = $('svg').attr('height');
-    expect(height).toEqual(barChartProps.height.toString());
+    expect(height).toEqual(componentProps.height.toString());
   });
 
   it('svg displays elements for all data values', () => {
     let valuesAllMatch = true;
     $('rect').toArray().forEach((rect, rectIndex) => {
-      const datumValue = barChartProps.data[rectIndex].value;
+      const datumValue = componentProps.data[rectIndex].value;
       const elementValue = $(rect).attr('value');
       if (datumValue != elementValue) { valuesAllMatch = false; }
     });
