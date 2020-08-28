4ac3ec88857cb337d18af1fff05d96e606faf340
August Sturm <auguststurm@users.noreply.github.com>
2020-08-27 23:07:33 -0700

    cleanup

--- src/BarChart/bar-chart.text.jsx
+++ src/BarChart/bar-chart.text.jsx
@@ -1,66 +1,8 @@
-// import { renderHook } from '@testing-library/react-hooks';
-// import { dataExampleSpecs } from '../../example/src/data';
-// import BarChart from '.';
-//
-// const { result } = renderHook(() => BarChart({...dataExampleSpecs.appComponentStates.barChart}));
-//
-//
-// describe('BarChart component', () => {
-//
-//   const height = result.current.props.children.props.height;
-//   const width = result.current.props.children.props.width;
-//
-//   it('should exist', () => {
-//     expect(result);
-//   });
-//
-//   it('should be type SVG', () => {
-//     expect(result.current.props.children.type).toEqual('svg');
-//   });
-//
-//   it(`should be ${height} high`, () => {
-//     expect(height).toEqual(dataExampleSpecs.appComponentStates.barChart.height);
-//   });
-//
-//   it(`should be ${width} wide`, () => {
-//     expect(width).toEqual(dataExampleSpecs.appComponentStates.barChart.width);
-//   });
-//
-// });
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-
-// import React from 'react';
-// import BarChart from '.';
-// import { dataExampleSpecs } from '../../example/src/data';
-// import { render } from '@testing-library/react';
-//
-// const component = (customProps = {}) => {
-//   const defaultProps = dataExampleSpecs.appComponentStates.barChart;
-//   const props = {...defaultProps, ...customProps};
-//   return render(<BarChart {...props} />);
-// }
-// // console.log(component());
-// // component().debug()
-//
-// describe('BarChart component', () => {
-//   it('should exist', () => {
-//     expect(component);
-//   });
-// });
-
-
-///////////////////////////////////////////////////////////////////////////////
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { dataExampleSpecs } from '../../example/src/data';
-import BarChart from '.';
-
 import cheerio from 'cheerio';
+import BarChart from '.';
 
 const barChartProps = dataExampleSpecs.appComponentStates.barChart;
 
