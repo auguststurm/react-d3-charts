c6a9ccce2423b8384c65ae92fb788a554df00217
August Sturm <auguststurm@users.noreply.github.com>
2020-08-28 13:37:03 -0700

    basic test setup

--- src/index.test.js
+++ src/index.test.js
@@ -4,3 +4,4 @@ import './BarChart/bar-chart.test.jsx';
 import './BarChartHorizontal/bar-chart-horizontal.text.jsx';
 import './PieChart/pie-chart.test.jsx';
 import './LineChart/line-chart.test.jsx';
+import './MultiLineChart/multi-line-chart.test.jsx';
