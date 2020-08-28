28a28f59797626d79d69110aef2e404ce42b5daa
August Sturm <auguststurm@users.noreply.github.com>
2020-08-28 13:05:18 -0700

    basic test setup

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 // import '@testing-library/jest-dom/extend-expect';
 
-import './BarChart/bar-chart.text.jsx';
+import './BarChart/bar-chart.test.jsx';
 import './BarChartHorizontal/bar-chart-horizontal.text.jsx';
 import './PieChart/pie-chart.test.jsx';
+import './LineChart/line-chart.test.jsx';
