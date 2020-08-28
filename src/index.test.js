00db0eac813ac25ffedf7e2f31f2300994a6e92e
August Sturm <auguststurm@users.noreply.github.com>
2020-08-28 14:37:57 -0700

    basic test setup

--- src/index.test.js
+++ src/index.test.js
@@ -7,3 +7,4 @@ import './LineChart/line-chart.test.jsx';
 import './MultiLineChart/multi-line-chart.test.jsx';
 import './StackedBarChart/stacked-bar-chart.test.jsx';
 import './StackedBarChartHorizontal/stacked-bar-chart-horizontal.test.jsx';
+import './EventsTimeline/events-timeline.test.jsx';
