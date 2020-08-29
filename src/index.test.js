fb6d40867e799978a4b88d7e80a9d69e856158ad
August Sturm <auguststurm@users.noreply.github.com>
2020-08-28 17:31:41 -0700

    fixed spelling

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 // import '@testing-library/jest-dom/extend-expect';
 
 import './BarChart/bar-chart.test.jsx';
-import './BarChartHorizontal/bar-chart-horizontal.text.jsx';
+import './BarChartHorizontal/bar-chart-horizontal.test.jsx';
 import './PieChart/pie-chart.test.jsx';
 import './LineChart/line-chart.test.jsx';
 import './MultiLineChart/multi-line-chart.test.jsx';
