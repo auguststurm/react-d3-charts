c9a94be6a13a8b4856d5a3e6c9f60cfe94553711
August Sturm <auguststurm@users.noreply.github.com>
2020-08-28 17:35:10 -0700

    consistency

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,7 @@ import './BarChart/bar-chart.test.jsx';
 import './BarChartHorizontal/bar-chart-horizontal.test.jsx';
 import './PieChart/pie-chart.test.jsx';
 import './LineChart/line-chart.test.jsx';
+import './HexbinChart/hexbin-chart.test.jsx';
 import './MultiLineChart/multi-line-chart.test.jsx';
 import './StackedBarChart/stacked-bar-chart.test.jsx';
 import './StackedBarChartHorizontal/stacked-bar-chart-horizontal.test.jsx';
